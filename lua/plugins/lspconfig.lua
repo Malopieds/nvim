df2bb912965983125be9df86508e36ed7cf2fda2
Malopieds <malobeauchamps@proton.me>
2026-02-17 19:19:37 +0100

    feat: spring boot hot tools

--- lua/plugins/lspconfig.lua
+++ lua/plugins/lspconfig.lua
@@ -28,6 +28,40 @@ return {
 							},
 						},
 					},
+					{
+						"elmcgill/springboot-nvim",
+						dependencies = {
+							"mfussenegger/nvim-jdtls",
+						},
+						config = function()
+							local springboot_nvim = require("springboot-nvim")
+							vim.keymap.set(
+								"n",
+								"<leader>Jr",
+								springboot_nvim.boot_run,
+								{ desc = "Spring Boot Run Project" }
+							)
+							vim.keymap.set(
+								"n",
+								"<leader>Jc",
+								springboot_nvim.generate_class,
+								{ desc = "Java Create Class" }
+							)
+							vim.keymap.set(
+								"n",
+								"<leader>Ji",
+								springboot_nvim.generate_interface,
+								{ desc = "Java Create Interface" }
+							)
+							vim.keymap.set(
+								"n",
+								"<leader>Je",
+								springboot_nvim.generate_enum,
+								{ desc = "Java Create Enum" }
+							)
+							springboot_nvim.setup({})
+						end,
+					},
 				},
 			},
 		},
@@ -89,35 +123,6 @@ return {
 				html = {
 					settings = {
 						filetype = { "html", "tsx", "typescript" },
-					},
-				},
-				tsserver = {
-					settings = {
-						typescript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayVariableTypeHints = true,
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-							},
-						},
-						javascript = {
-							inlayHints = {
-								includeInlayParameterNameHints = "all", -- 'none' | 'literals' | 'all'
-								includeInlayParameterNameHintsWhenArgumentMatchesName = true,
-								includeInlayVariableTypeHints = true,
-
-								includeInlayFunctionParameterTypeHints = true,
-								includeInlayVariableTypeHintsWhenTypeMatchesName = true,
-								includeInlayPropertyDeclarationTypeHints = true,
-								includeInlayFunctionLikeReturnTypeHints = true,
-								includeInlayEnumMemberValueHints = true,
-							},
-						},
 					},
 				},
 				tailwindcss = {},
